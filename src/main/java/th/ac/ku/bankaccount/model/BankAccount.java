08568453922c9c44e86e3475ff962f4b1480ae7a
Winternight9 <poomiix9@gmail.com>
2020-10-05 13:36:37 +0700

    customer api

---
+++ src/main/java/th/ac/ku/bankaccount/model/BankAccount.java
@@ -0,0 +1,53 @@
+package th.ac.ku.bankaccount.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class BankAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int customerId;
+    private String type;
+    private double balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return this.customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" + "id=" + id + ", type=" + type + '\'' +
+                ", balance=" + balance + '\'' +
+                '}';
+    }
+}
